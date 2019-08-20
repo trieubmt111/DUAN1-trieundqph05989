ac016a67ac292497419c1060da321b09ea8cb416
trieubmt111 <trieubmt111@gmail.com>
2019-08-20 17:14:45 +0700

    Create HibernateUtils.java

---
+++ src/duan1_trieundqph05989/util/HibernateUtils.java
@@ -0,0 +1,22 @@
+package duan1_trieundqph05989.util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+	private static final SessionFactory sessionFactory;
+
+	static {
+		try {
+			sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}

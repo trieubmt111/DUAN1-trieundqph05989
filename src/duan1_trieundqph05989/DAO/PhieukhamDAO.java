ede17b1ec2661eee617f78fdf7382babc225e2f1
trieubmt111 <trieubmt111@gmail.com>
2019-08-20 17:14:18 +0700

    Create PhieukhamDAO.java

---
+++ src/duan1_trieundqph05989/DAO/PhieukhamDAO.java
@@ -0,0 +1,18 @@
+package duan1_trieundqph05989.DAO;
+
+import java.util.List;
+
+import duan1_trieundqph05989.entity.Phieukham;
+
+public interface PhieukhamDAO {
+	public List<Phieukham> getAlls(int mapik);
+
+    public boolean insert(Phieukham phieukham);
+
+    public boolean update(Phieukham phieukham);
+
+    public boolean delete(int mapik);
+
+    public Phieukham getPhieukhamById(int mapik);
+
+}
